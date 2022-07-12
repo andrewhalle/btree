58b981b44b86914c14908728bb2e3926491709c7
Andrew Halle <me@andrewhalle.dev>
2022-07-11 21:06:03 -0700

    Add `insert_if_space` function
    **This commit**
    * Add `insert_if_space` function.
    * add a capacity parameter to `Node::new`
    
    **Why?**
    This function will be needed to check if we can insert into a node or
    whether it needs to be split and a new parent added.

--- examples/simple.rs
+++ examples/simple.rs
@@ -6,11 +6,11 @@ use btree::Node;
 fn main() {
     let filename = PathBuf::from(env::var("BTREE_FILENAME").expect("BTREE_FILENAME not set"));
 
-    let mut node = Node::new(filename).expect("Could not create node.");
-    node.insert(0, "hello");
-    node.insert(1, "world");
+    let mut node = Node::new(filename, 2).expect("Could not create node.");
+    let _ = node.insert_if_space(0, "hello");
+    let _ = node.insert_if_space(1, "world");
     node.save().expect("Could not save node.");
 
-    node.insert(2, "hello!");
+    let _ = dbg!(node.insert_if_space(2, "hello!"));
     node.save().expect("Could not save node.");
 }
