926d92be1cbbaaf9606ecf0892e735f5b95faff4
Andrew Halle <me@andrewhalle.dev>
2022-07-15 19:45:39 -0700

    Add `struct BTree`.
    **This commit**
    * Make `Node` methods private.
    * rename `enum Error` to `enum NodeError` and make it private.
    * Add `struct BTree` and `enum Error`.
    * Add `BTree::new()`.
    
    **Why?**
    Get started on actual BTree functionality using Nodes.

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,16 +1,10 @@
 use std::env;
 use std::path::PathBuf;
 
-use btree::Node;
+use btree::BTree;
 
 fn main() {
-    let filename = PathBuf::from(env::var("BTREE_FILENAME").expect("BTREE_FILENAME not set"));
+    let dir = PathBuf::from(env::var("BTREE_FILENAME").expect("BTREE_FILENAME not set"));
 
-    let mut node = Node::new(filename, 2).expect("Could not create node.");
-    let _ = node.insert_if_space(0, "hello");
-    let _ = node.insert_if_space(1, "world");
-    node.save().expect("Could not save node.");
-
-    let _ = dbg!(node.insert_if_space(2, "hello!"));
-    node.save().expect("Could not save node.");
+    let _tree: BTree<String, String> = BTree::new(dir, 16).expect("Could not create BTree.");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,41 @@
-use std::fs::{File, OpenOptions};
+use std::error::Error as StdError;
+use std::fs::{DirBuilder, File, OpenOptions};
 use std::io::{self, Seek};
 use std::path::PathBuf;
 
 use rmp_serde::Serializer;
 use serde::{Deserialize, Serialize};
-use thiserror::Error;
+use uuid::Uuid;
 
 type NodeRef = PathBuf;
 
-pub struct Node<K, V> {
+pub struct BTree<K, V> {
+    root_node: Node<K, V>,
+    dir: PathBuf,
+}
+
+struct Node<K, V> {
     file: File,
     data: NodeData<K, V>,
 }
 
 #[derive(Deserialize, Serialize)]
-pub struct NodeData<K, V> {
+struct NodeData<K, V> {
     keys: Vec<K>,
     values: Vec<V>,
     children: Vec<NodeRef>,
 }
 
-#[derive(Error, Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum Error {
+    #[error("An I/O error occurred.")]
+    Io(#[from] io::Error),
+    #[error("A serialization error occurred.")]
+    Serialization(#[from] rmp_serde::encode::Error),
+}
+
+#[derive(thiserror::Error, Debug)]
+enum NodeError {
     #[error("Cannot insert into the node because it is too full.")]
     NeedsSplit,
     #[error("An I/O error occurred.")]
@@ -30,19 +44,40 @@ pub enum Error {
     Serialization(#[from] rmp_serde::encode::Error),
 }
 
+impl<K, V> BTree<K, V>
+where
+    K: Serialize + Ord,
+    V: Serialize,
+{
+    pub fn new(dir: PathBuf, capacity: usize) -> Result<Self, Error> {
+        DirBuilder::new().create(&dir)?;
+        let mut root_node = dir.clone();
+        root_node.push(Uuid::new_v4().to_string());
+        let root_node = match Node::new(root_node, capacity) {
+            Ok(node) => node,
+            Err(NodeError::Io(e)) => {
+                return Err(Error::Io(e));
+            }
+            _ => unreachable!(),
+        };
+
+        Ok(Self { root_node, dir })
+    }
+}
+
 impl<K, V> Node<K, V>
 where
     K: Serialize + Ord,
     V: Serialize,
 {
-    fn reset_file(&mut self) -> Result<(), Error> {
+    fn reset_file(&mut self) -> Result<(), NodeError> {
         self.file.set_len(0)?;
         self.file.rewind()?;
 
         Ok(())
     }
 
-    pub fn new(path: PathBuf, capacity: usize) -> Result<Self, Error> {
+    pub fn new(path: PathBuf, capacity: usize) -> Result<Self, NodeError> {
         let file = OpenOptions::new()
             .read(true)
             .write(true)
@@ -57,7 +92,7 @@ where
         Ok(Node { file, data })
     }
 
-    pub fn save(&mut self) -> Result<(), Error> {
+    pub fn save(&mut self) -> Result<(), NodeError> {
         self.reset_file()?;
 
         self.data.serialize(&mut Serializer::new(&mut self.file))?;
@@ -65,7 +100,7 @@ where
         Ok(())
     }
 
-    pub fn insert_if_space(&mut self, key: K, value: V) -> Result<(), Error> {
+    pub fn insert_if_space(&mut self, key: K, value: V) -> Result<(), NodeError> {
         let idx = &self.data.keys[..].binary_search(&key);
 
         match idx {
@@ -76,7 +111,7 @@ where
                     self.data.values.insert(*idx, value);
                     Ok(())
                 } else {
-                    Err(Error::NeedsSplit)
+                    Err(NodeError::NeedsSplit)
                 }
             }
         }
